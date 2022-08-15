2597ba5a2ca3f19e9451d2d76d77ad06b782dcee
rodrigues-i <lucas-f250@live.com>
2022-08-15 12:37:53 -0300

    Change the User entity fields to allow null value

--- Models/User.cs
+++ Models/User.cs
@@ -3,11 +3,11 @@ namespace CrudClientes.Models;
 public class User
 {
     // o id deveria ser uma string
-    public int id {get; set;}
+    public int? id {get; set;}
     public string? firstName {get; set;}
     public string? surname {get; set;}
-    public int age {get; set; }
+    public int? age {get; set; }
 
-    public DateTime creationDate { get; set; }
+    public DateTime? creationDate { get; set; }
 
 }
