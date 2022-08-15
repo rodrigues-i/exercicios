c786b338ba82343781a816cb8664f972f9037915
rodrigues-i <lucas-f250@live.com>
2022-08-15 11:07:48 -0300

    Create the UserRepository to implement the interface IUserRepository

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,45 @@
+using CrudClientes.Models;
+using CrudClientes.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudClientes.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly DataContext _context;
+
+    public UserRepository(DataContext context)
+    {
+        _context = context;
+        
+    }
+    public void AddUser(User user)
+    {
+        _context.Add(user);
+    }
+
+    public void DeleteUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> GetUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public void UpdateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
