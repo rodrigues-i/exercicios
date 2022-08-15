773b7dc4d1a9d838d8e49b0ebe16997c54ec86bd
rodrigues-i <lucas-f250@live.com>
2022-08-15 17:49:38 -0300

    Mark firstName as required, surname as nullable and everything else non-nullable

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CrudClientes.Models;
 
 public class User
 {
     // o id deveria ser uma string
-    public int? id {get; set;}
-    public string? firstName {get; set;}
+    public int id {get; set;}
+    [Required]
+    public string firstName {get; set;}
     public string? surname {get; set;}
-    public int? age {get; set; }
+    public int age {get; set; }
 
-    public DateTime? creationDate { get; set; }
+    public DateTime creationDate { get; set; }
 
 }
