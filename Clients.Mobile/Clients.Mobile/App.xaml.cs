a1da1506dcfaf69efaf154eb760fc7eb636bce89
rodrigues-i <lucas-f250@live.com>
2022-09-11 20:58:16 -0300

    Turn the MainPage into a NavigationPage

--- Clients.Mobile/Clients.Mobile/App.xaml.cs
+++ Clients.Mobile/Clients.Mobile/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Clients.Mobile
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
