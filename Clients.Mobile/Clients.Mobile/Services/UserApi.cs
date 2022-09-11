03af29375deefe52c55e3595efe8e660655506dc
rodrigues-i <lucas-f250@live.com>
2022-09-11 20:24:08 -0300

    Create a class to handle api requests

---
+++ Clients.Mobile/Clients.Mobile/Services/UserApi.cs
@@ -0,0 +1,37 @@
+﻿using Clients.Mobile.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Clients.Mobile.Services
+{
+    public class UserApi
+    {
+        const String URL = "http://172.17.224.1:5000/Users";
+
+        private HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "Application/json");
+            client.DefaultRequestHeaders.Add("Connection", "close");
+
+            return client;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(URL);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<User>>(content);
+            }
+
+            return new List<User>();
+        }
+    }
+}
