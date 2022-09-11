0318d6cc4c80bb24098209b6bfba3dfb23fc2f1f
rodrigues-i <lucas-f250@live.com>
2022-09-11 19:49:16 -0300

    Create a model class

---
+++ Clients.Mobile/Clients.Mobile/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clients.Mobile.Model
+{
+    public class User
+    {
+        private Guid Id { get; }
+        private string FirstName { get; set; }
+        private string Surname { get; set; }
+        private int Age { get; set; }
+        private DateTime CreationDate { get; set; }
+
+    }
+}
