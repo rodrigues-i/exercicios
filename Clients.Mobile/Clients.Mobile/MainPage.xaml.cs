131eb863dbc265769f79fc7550b73660d6ce52eb
rodrigues-i <lucas-f250@live.com>
2022-09-11 21:03:03 -0300

    Add the code which will handle the navigation

--- Clients.Mobile/Clients.Mobile/MainPage.xaml.cs
+++ Clients.Mobile/Clients.Mobile/MainPage.xaml.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Clients.Mobile.Model;
+using Clients.Mobile.Services;
+using System;
 using Xamarin.Forms;
 
 namespace Clients.Mobile
 {
     public partial class MainPage : ContentPage
     {
+        private User user;
+        private UserApi api;
         public MainPage()
         {
             InitializeComponent();
+            api = new UserApi();
+        }
+
+        private void MyButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BuscaTodosUsuarios());
         }
     }
 }
