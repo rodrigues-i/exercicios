33b436519224ed88f27f6284ab75e4bced47a63c
rodrigues-i <lucas-f250@live.com>
2022-09-14 08:42:32 -0300

    Implement the event handler which takes the data from the entries and use them in the api request

--- Clients.Mobile/Clients.Mobile/CriaUsuario.xaml.cs
+++ Clients.Mobile/Clients.Mobile/CriaUsuario.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Clients.Mobile.Model;
+using Clients.Mobile.Services;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +10,59 @@ namespace Clients.Mobile
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CriaUsuario : ContentPage
     {
-        public CriaUsuario()
+        private readonly UserApi _api;
+        public CriaUsuario(UserApi api)
         {
+            _api = api;
             InitializeComponent();
+            Title = "Criando novo usuário";
+        }
+
+        private async void btCriaUsuario_Clicked(object sender, EventArgs e)
+        {
+            var firstName = entFirstName.Text;
+            var surname = entSurname.Text;
+            int age = 0;
+
+            if(entAge.Text == null)
+            {
+                await DisplayAlert("Alerta", "O campo idade é obrigatório", "Ok");
+            }
+            else if(!int.TryParse(entAge.Text.Trim(), out age))
+            {
+                await DisplayAlert("Alerta", "Informe uma idade válida", "Ok");
+            }
+            else if (firstName == null || firstName.Trim() == "")
+            {
+                await DisplayAlert("Alerta", "O campo nome é obrigatório", "Ok");
+            }
+            else
+            {
+                var user = new User
+                {
+                    FirstName = firstName,
+                    Surname = surname,
+                    Age = age
+                };
+
+                try
+                {
+                    await _api.CreateUser(user);
+                    await DisplayAlert("Alerta", "Usuário criado com sucesso!", "Ok");
+                    LimparCampos();
+                }
+                catch (Exception error)
+                {
+                    await DisplayAlert("Erro", error.Message, "Ok");
+                }
+            }
+        }
+
+        private void LimparCampos()
+        {
+            entFirstName.Text = "";
+            entSurname.Text = "";
+            entAge.Text = "";
         }
     }
 }
