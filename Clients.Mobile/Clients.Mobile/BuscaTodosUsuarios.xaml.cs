630d78aed748ca9d71344380359e9ab39570d34b
rodrigues-i <lucas-f250@live.com>
2022-09-16 17:16:17 -0300

    Create a button to copy the id to the clipboard

--- Clients.Mobile/Clients.Mobile/BuscaTodosUsuarios.xaml.cs
+++ Clients.Mobile/Clients.Mobile/BuscaTodosUsuarios.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Clients.Mobile.Model;
 using System.Collections.Generic;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -21,9 +22,29 @@ namespace Clients.Mobile
         {
             base.OnAppearing();
             Title = "Usuários";
+            
+            if(users.Count == 0)
+            {
+                DisplayAlert("Alerta", "Não há usuários no sistema, volte e adicione um usuário na página de criar usuário.", "Ok");
+            }
+            else
+            {
+                usersListView.ItemsSource = users;
+                usersListView.HasUnevenRows = true;  // Expande a altura, assim todos os items são exibidos
+            }
+        }
 
-            usersListView.ItemsSource = users;
-            usersListView.HasUnevenRows = true;  // Expande a altura, assim todos os items são exibidos
+        private async void btCopia_Id_Clicked(object sender, System.EventArgs e)
+        {
+            var button = (Button)sender;
+            var id = button.ClassId;
+
+            // Copy id to clipboard
+            await Clipboard.SetTextAsync(id);
+            if(Clipboard.HasText && await Clipboard.GetTextAsync() == id)
+            {
+                await DisplayAlert("Alerta", "Id copiado para o clipboard", "Ok");
+            }
         }
     }
 }
